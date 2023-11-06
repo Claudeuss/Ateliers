7ddeb67fa137b0545c44921534e89348021d1aee
Claudeuss <133555372+Claudeuss@users.noreply.github.com>
2023-11-06 16:29:26 +0700

    Update serviceform.dart
    Add ui service form

--- lib/serviceform.dart
+++ lib/serviceform.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ServiceForm extends StatefulWidget {
   const ServiceForm({super.key});
@@ -11,6 +14,141 @@ class ServiceForm extends StatefulWidget {
 class _ServiceFormState extends State<ServiceForm> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        extendBody: true,
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.only(top: 30),
+            constraints: BoxConstraints(
+                maxHeight: MediaQuery.of(context).size.height,
+                maxWidth: MediaQuery.of(context).size.width),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.blue.shade600, Colors.blue.shade300],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter)),
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                            size: 20,
+                          )),
+                      Container(
+                        height: 120,
+                        width: double.infinity,
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/landing.png'),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                    flex: 7,
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                              BorderRadius.vertical(top: Radius.circular(20))),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Form Service",
+                            style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500)),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                                hintText: 'Your Name',
+                                icon: Icon(
+                                  CupertinoIcons.person_crop_circle,
+                                  size: 40,
+                                ),
+                                filled: true,
+                                fillColor: Colors.grey[300],
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 5, horizontal: 15),
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius: BorderRadius.circular(30))),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                                hintText: 'Motorcycle',
+                                icon: Icon(
+                                  Icons.motorcycle_outlined,
+                                  size: 40,
+                                ),
+                                filled: true,
+                                fillColor: Colors.grey[300],
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 5, horizontal: 15),
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius: BorderRadius.circular(30))),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                                hintText: 'Address',
+                                icon: Icon(
+                                  Icons.location_pin,
+                                  size: 40,
+                                ),
+                                filled: true,
+                                fillColor: Colors.grey[300],
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 5, horizontal: 15),
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius: BorderRadius.circular(30))),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          ElevatedButton(
+                              onPressed: () {},
+                              child: Container(
+                                decoration:
+                                    BoxDecoration(color: Colors.blue.shade600),
+                              ))
+                        ],
+                      ),
+                    ))
+              ],
+            ),
+          ),
+        ));
   }
 }
